32e81d6882ae8708dc1ecb02c995d131b80ada02
muna bedan <munabedan@gmail.com>
2020-06-05 07:04:21 +0300

    [done] added ionic framework to about page
    Reason:
    -got rid of all css

---
+++ scripting/about.js
@@ -0,0 +1,14 @@
+//twitter
+function openTwitter() {
+    window.open("https://twitter.com/login")
+}
+
+//instagram
+function openInstagram() {
+    window.open("https://www.instagram.com/accounts/login/?next=%2F&source=logged_out_half_sheet")
+}
+
+//youtube
+function openYoutube() {
+    window.open("https://youtube.com")
+}
